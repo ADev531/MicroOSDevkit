146e4bd0f398c90557cbd8ffe251ebf888c84677
adev <battlegom@outlook.kr>
2023-08-10 19:41:38 +0900

    kernel.cpp Drivers/Keyboard/ IOPorts.h

--- kernel.cpp
+++ kernel.cpp
@@ -1,9 +1,11 @@
 #include "Library/io.h"
 #include "Drivers/VGAScreen.hpp"
 #include "Library/memory.h"
+#include "Drivers/Keyboard/keyboard.hpp"
 
 int main() {
     Print("Hello, world!\n");
+    VGAConsole::PrintChar(GetKey());
     while (1);
     return 0;
 }
